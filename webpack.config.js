083783682a77a2d823cd40b4246ace9e5e7201af
Bill Kilmer <wdk122@gmail.com>
2016-10-11 17:30:50 -0700

    Adds environment configurations.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,52 @@
-const NODE_ENV = process.env.NODE_ENV;
-const isDev = NODE_ENV === 'development';
-// alternatively, we can use process.argv[1]
-// const isDev = (process.argv[1] || '')
-//                .indexOf('hjs-dev-server') !== -1;
-
-const webpack = require('webpack');
-const fs      = require('fs');
-const path    = require('path'),
-      join    = path.join,
-      resolve = path.resolve;
-
 const getConfig = require('hjs-webpack');
+const dotenv    = require('dotenv');
+const webpack   = require('webpack');
+const fs        = require('fs');
+const path      = require('path'),
+      join      = path.join,
+      resolve   = path.resolve;
 
 const root    = resolve(__dirname);
 const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
 const dest    = join(root, 'dist');
 
-var config = getConfig({
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
+
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path:   join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
+});
+
+const envVariables =
+  Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+    .reduce((memo, key) => {
+      const val = JSON.stringify(envVariables[key]);
+      memo[`__${key.toUpperCase()}__`] = val;
+      return memo;
+    }, {
+      __NODE_ENV__: JSON.stringify(NODE_ENV)
+    });
+
+// tutorial says var config
+const config = getConfig({
   isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
   clearBeforeBuild: true
 });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
 
 config.postcss = [].concat([
   require('precss')({}),
